4c552c6ba4871bd3a2364fa6355316191912044a
Hung <hung@thinklabs.vn>
2020-04-05 22:26:55 +0700

    cjonfi

--- app/database/index.js
+++ app/database/index.js
@@ -6,25 +6,27 @@ var logger 		= require('../logger');
 
 // Connect to the database
 // construct the database URI and encode username and password.
-var dbURI = config.db.fullUri || ("mongodb://" + 
-			encodeURIComponent(config.db.username) + ":" + 
-			encodeURIComponent(config.db.password) + "@" + 
-			config.db.host + ":" + 
-			config.db.port + "/" + 
+var dbURI = config.db.fullUri || ("mongodb://" +
+			encodeURIComponent(config.db.username) + ":" +
+			encodeURIComponent(config.db.password) + "@" +
+			config.db.host + ":" +
+			config.db.port + "/" +
 			config.db.name + "?ssl=true");
+console.log(dbURI);
 Mongoose.connect(dbURI, { useNewUrlParser: true });
 
 // Throw an error if the connection fails
 Mongoose.connection.on('error', function(err) {
 	if(err) throw err;
+	console.log("Connect to DB Successful")
 });
 
-// mpromise (mongoose's default promise library) is deprecated, 
+// mpromise (mongoose's default promise library) is deprecated,
 // Plug-in your own promise library instead.
 // Use native promises
 Mongoose.Promise = global.Promise;
 
-module.exports = { Mongoose, 
+module.exports = { Mongoose,
 	models: {
 		user: require('./schemas/user.js'),
 		room: require('./schemas/room.js')
