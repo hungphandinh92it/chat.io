96db76fb54051a131b46f498e4d8eeeed264e3da
Hung Phan Dinh <hung@thinklabs.vn>
2020-04-05 10:20:40 +0000

    config

--- app/config/index.js
+++ app/config/index.js
@@ -1,35 +1,35 @@
 'use strict';
 
 var init = function () {
-
+	var defaultConfig = require('./config.json');
 	if(process.env.NODE_ENV === 'production') {
 		var redisURI 		= require('url').parse(process.env.REDIS_URL);
 		var redisPassword 	= redisURI.auth.split(':')[1];
 		return {
 			db: {
-				username: process.env.dbUsername,
-				password: process.env.dbPassword,
-				host: process.env.dbHost,
-				port: process.env.dbPort,
-				name: process.env.dbName
+				username: process.env.dbUsername || defaultConfig.db.username,
+				password: process.env.dbPassword || defaultConfig.db.dbPassword,
+				host: process.env.dbHost || defaultConfig.db.dbHost,
+				port: process.env.dbPort || defaultConfig.db.dbPort,
+				name: process.env.dbName || defaultConfig.db.dbName
 			},
-			sessionSecret: process.env.sessionSecret,
+			sessionSecret: process.env.sessionSecret || defaultConfig.sessionSecret,
 			facebook: {
-				clientID: process.env.facebookClientID,
-				clientSecret: process.env.facebookClientSecret,
+				clientID: process.env.facebookClientID || defaultConfig.facebook.facebookClientID,
+				clientSecret: process.env.facebookClientSecret || defaultConfig.facebook.facebookClientSecret,
 				callbackURL: "/auth/facebook/callback",
 				profileFields: ['id', 'displayName', 'photos']
 			},
 			twitter:{
-				consumerKey: process.env.twitterConsumerKey,
-				consumerSecret: process.env.twitterConsumerSecret,
+				consumerKey: process.env.twitterConsumerKey || defaultConfig.twitter.twitterConsumerKey,
+				consumerSecret: process.env.twitterConsumerSecret || defaultConfig.twitter.twitterConsumerSecret,
 				callbackURL: "/auth/twitter/callback",
 				profileFields: ['id', 'displayName', 'photos']
 			},
 			redis: {
-				host: redisURI.hostname,
-				port: redisURI.port,
-				password: redisPassword
+				host: redisURI.hostname || defaultConfig.redis.hostname,
+				port: redisURI.port || defaultConfig.redis.port,
+				password: redisPassword || defaultConfig.redis.redisPassword
 			}
 		}
 	}
