63bb528e6a1c7dfcc843c7f2486325a8d773b12e
Hung <hung@thinklabs.vn>
2020-04-06 08:22:08 +0700

    cjonfi

--- app/config/index.js
+++ app/config/index.js
@@ -1,42 +1,41 @@
 'use strict';
 
 var init = function () {
-	var defaultConfig = require('./config.json');
-	if(process.env.NODE_ENV === 'production') {
-		var redisURI 		= require('url').parse(process.env.REDIS_URL);
-		var redisPassword 	= redisURI.auth.split(':')[1];
-		return {
-			db: {
-				username: process.env.dbUsername || defaultConfig.db.username,
-				password: process.env.dbPassword || defaultConfig.db.dbPassword,
-				host: process.env.dbHost || defaultConfig.db.dbHost,
-				port: process.env.dbPort || defaultConfig.db.dbPort,
-				name: process.env.dbName || defaultConfig.db.dbName,
-				fullUri: process.env.fullDBUri
-			},
-			sessionSecret: process.env.sessionSecret || defaultConfig.sessionSecret,
-			facebook: {
-				clientID: process.env.facebookClientID || defaultConfig.facebook.facebookClientID,
-				clientSecret: process.env.facebookClientSecret || defaultConfig.facebook.facebookClientSecret,
-				callbackURL: "/auth/facebook/callback",
-				profileFields: ['id', 'displayName', 'photos']
-			},
-			twitter:{
-				consumerKey: process.env.twitterConsumerKey || defaultConfig.twitter.twitterConsumerKey,
-				consumerSecret: process.env.twitterConsumerSecret || defaultConfig.twitter.twitterConsumerSecret,
-				callbackURL: "/auth/twitter/callback",
-				profileFields: ['id', 'displayName', 'photos']
-			},
-			redis: {
-				host: redisURI.hostname || defaultConfig.redis.hostname,
-				port: redisURI.port || defaultConfig.redis.port,
-				password: redisPassword || defaultConfig.redis.redisPassword
-			}
-		}
-	}
-	else {
-		return require('./config.json');
-	}
+  var defaultConfig = require('./config.json');
+  if (process.env.NODE_ENV === 'production') {
+    var redisURI = require('url').parse(process.env.REDIS_URL);
+    var redisPassword = redisURI.auth.split(':')[1];
+    return {
+      db: {
+        username: process.env.dbUsername || defaultConfig.db.username,
+        password: process.env.dbPassword || defaultConfig.db.dbPassword,
+        host: process.env.dbHost || defaultConfig.db.dbHost,
+        port: process.env.dbPort || defaultConfig.db.dbPort,
+        name: process.env.dbName || defaultConfig.db.dbName,
+        fullUri: process.env.fullDBUri
+      },
+      sessionSecret: process.env.sessionSecret || defaultConfig.sessionSecret,
+      facebook: {
+        clientID: process.env.facebookClientID || defaultConfig.facebook.facebookClientID,
+        clientSecret: process.env.facebookClientSecret || defaultConfig.facebook.facebookClientSecret,
+        callbackURL: "/auth/facebook/callback",
+        profileFields: ['id', 'displayName', 'photos']
+      },
+      twitter: {
+        consumerKey: process.env.twitterConsumerKey || defaultConfig.twitter.twitterConsumerKey,
+        consumerSecret: process.env.twitterConsumerSecret || defaultConfig.twitter.twitterConsumerSecret,
+        callbackURL: "/auth/twitter/callback",
+        profileFields: ['id', 'displayName', 'photos']
+      },
+      redis: {
+        host: redisURI.hostname || defaultConfig.redis.hostname,
+        port: redisURI.port || defaultConfig.redis.port,
+        password: redisPassword || defaultConfig.redis.redisPassword
+      }
+    }
+  } else {
+    return require('./config.json');
+  }
 }
 
 module.exports = init();
--- app/database/index.js
+++ app/database/index.js
@@ -1,24 +1,24 @@
 'use strict';
 
-var config 		= require('../config');
-var Mongoose 	= require('mongoose');
-var logger 		= require('../logger');
+var config = require('../config');
+var Mongoose = require('mongoose');
+var logger = require('../logger');
 
 // Connect to the database
 // construct the database URI and encode username and password.
-var dbURI = config.db.fullUri || ("mongodb://" +
-			encodeURIComponent(config.db.username) + ":" +
-			encodeURIComponent(config.db.password) + "@" +
-			config.db.host + ":" +
-			config.db.port + "/" +
-			config.db.name + "?ssl=true");
-console.log(dbURI);
-Mongoose.connect(dbURI, { useNewUrlParser: true });
+// var dbURI = config.db.fullUri || ("mongodb://" +
+// 			encodeURIComponent(config.db.username) + ":" +
+// 			encodeURIComponent(config.db.password) + "@" +
+// 			config.db.host + ":" +
+// 			config.db.port + "/" +
+// 			config.db.name + "?ssl=true");
+var dbURI = process.env.fullDBUri;
+Mongoose.connect(dbURI, {useNewUrlParser: true});
 
 // Throw an error if the connection fails
-Mongoose.connection.on('error', function(err) {
-	if(err) throw err;
-	console.log("Connect to DB Successful")
+Mongoose.connection.on('error', function (err) {
+  if (err) throw err;
+  console.log("Connect to DB Successful")
 });
 
 // mpromise (mongoose's default promise library) is deprecated,
@@ -26,9 +26,10 @@ Mongoose.connection.on('error', function(err) {
 // Use native promises
 Mongoose.Promise = global.Promise;
 
-module.exports = { Mongoose,
-	models: {
-		user: require('./schemas/user.js'),
-		room: require('./schemas/room.js')
-	}
+module.exports = {
+  Mongoose,
+  models: {
+    user: require('./schemas/user.js'),
+    room: require('./schemas/room.js')
+  }
 };
